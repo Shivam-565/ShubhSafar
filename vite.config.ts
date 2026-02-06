59994552b7d11f3fc348d670c906fe4f4bf2953f
Shivam Rai <152804649+Shivam-565@users.noreply.github.com>
2026-02-07 02:35:45 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  // ADD THIS SECTION
+  build: {
+    outDir: 'dist', 
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
